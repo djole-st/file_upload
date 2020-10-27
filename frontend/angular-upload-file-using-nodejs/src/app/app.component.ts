de21284df218128f7159ab1e6878dbaddf8c66ac
Djordje Stankovic <djordje.stankovic87@gmail.com>
2020-10-27 22:48:35 +0100

    m

---
+++ frontend/angular-upload-file-using-nodejs/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-image-file-upload-tutorial';
+
+  @ViewChild('UploadFileInput', { static: false }) uploadFileInput: ElementRef;
+  fileUploadForm: FormGroup;
+  fileInputLabel: string;
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.fileUploadForm = this.formBuilder.group({
+      uploadedImage: ['']
+    });
+  }
+
+  onFileSelect(event) {
+    const file = event.target.files[0];
+    this.fileInputLabel = file.name;
+    this.fileUploadForm.get('uploadedImage').setValue(file);
+  }
+
+
+  onFormSubmit() {
+
+    if (!this.fileUploadForm.get('uploadedImage').value) {
+      alert('Please fill valid details!');
+      return false;
+    }
+
+    const formData = new FormData();
+    formData.append('uploadedImage', this.fileUploadForm.get('uploadedImage').value);
+    formData.append('agentId', '007');
+
+
+    this.http
+      .post<any>('http://localhost:3000/uploadfile', formData).subscribe(response => {
+        console.log(response);
+        if (response.statusCode === 200) {
+          // Reset the file input
+          this.uploadFileInput.nativeElement.value = "";
+          this.fileInputLabel = undefined;
+        }
+      }, er => {
+        console.log(er);
+        alert(er.error.error);
+      });
+  }
+
+
+}
